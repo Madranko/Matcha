008f17042b3fec00178ce77838d7fbe8ba0aa965
Dmytro POLOSUKHIN <dpolosuk@e1r2p7.unit.ua>
2018-05-14 16:47:06 +0300

    routes works

--- public/index.php
+++ public/index.php
@@ -1,7 +1,24 @@
 <?php
-    define('ROOT', __DIR__ . "/../");
-    require '../vendor/autoload.php';
-    include_once ROOT . '/config/settings.php';
-    $app = new \Slim\App($settings);
-    include_once ROOT . '/config/pdo_connection.php';
+
+define('ROOT', __DIR__ . "/../");
+
+require '../vendor/autoload.php';
+
+include_once ROOT . '/config/settings.php';
+
+$app = new \Slim\App($settings);
+
+include_once ROOT . '/config/pdo_connection.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$container['view'] = new \Slim\Views\PhpRenderer('../public/views');
+
+$app->get('/hello', function (Request $request, Response $response, array $args) {
+	return $this->view->render($response, 'index.html');
+});
+
+$app->run();
+
 ?>
