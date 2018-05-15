2fe8c44d88cb8ba5ef531b6e8e2af9f5f24b8c17
Dmytro POLOSUKHIN <dpolosuk@e2r4p10.unit.ua>
2018-05-15 11:19:19 +0300

    added blade and rewrite index.blade.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,13 @@
 <?php
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-    $container['view'] = new \Slim\Views\PhpRenderer(ROOT . 'public/views');
+$app->get('/', function (Request $request, Response $response, array $args) use ($blade) {
+	echo $blade->make('index', [
+		'name' => 'John Doe',
+		'value' => 'bla bla'
+	]);
+});
 
-    $app->get('/', function (Request $request, Response $response, array $args) {
-        return $this->view->render($response, 'index.html');
-    });
 ?>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 define('ROOT', __DIR__ . "/../");
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Jenssegers\Blade\Blade;
+
 require ROOT . '/vendor/autoload.php';
 
 include_once ROOT . '/config/settings.php';
@@ -9,6 +13,8 @@ include_once ROOT . '/config/settings.php';
 $app = new \Slim\App($settings);
 
 include_once ROOT . '/config/pdo_connection.php';
+
+$blade = new Blade('views', 'cache');
 
 include ROOT . '/app/Http/routes.php';
 
