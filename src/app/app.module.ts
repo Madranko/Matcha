61488fe309b3a549f525034af4e22fd447b1ccec
Dmytro POLOSUKHIN <dpolosuk@e1r2p7.unit.ua>
2018-05-18 17:41:35 +0300

    some features work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		MatButtonModule,
+		MatIconModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
